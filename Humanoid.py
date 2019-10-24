corrected joint rotation directions, implemented low pass filter, added torque limit

Humanoid.py
@@ -23,12 +23,14 @@ class Humanoid(HumanoidBase):
         # wartosci, ktore beda wyslane do dynamixelli
         self.dynamixel_values = [0.0] * 20
         self.dimensions = {'arm': 1.0, 'forearm': 1.0, 'shoulder': 0.5, 'spine': 1}
+        max_torque = [255] * 20
+        self.sync_write_2(self.ADR_TOR_LIM, max_torque)
 
-    def send_signals(self, k=0.1):
+    def send_signals(self, k=0.05):
         # ustawienie wartosci wywyslanych do dynamixeli, z filtrem low pass
         self.dynamixel_values[13] = self.dynamixel_values[13]*(1-k) + k*(512 - int(self.angles['RAy']*1024/np.deg2rad(300)))
         self.dynamixel_values[14] = self.dynamixel_values[14]*(1-k) + k*(512 - int(self.angles['RAx'] * 1024 / np.deg2rad(300)))
-        self.dynamixel_values[15] = self.dynamixel_values[14]*(1-k) + k*(512 + int(self.angles['RFAy'] * 1024 / np.deg2rad(300)))
+        self.dynamixel_values[15] = self.dynamixel_values[15]*(1-k) + k*(512 + int(self.angles['RFAy'] * 1024 / np.deg2rad(300)))
 
         self.dynamixel_values[16] = self.dynamixel_values[16]*(1-k) + k*(512 + int(self.angles['LAy'] * 1024 / np.deg2rad(300)))
         self.dynamixel_values[17] = self.dynamixel_values[17]*(1-k) + k*(512 - int(self.angles['LAx'] * 1024 / np.deg2rad(300)))
@@ -36,7 +38,8 @@ class Humanoid(HumanoidBase):
 
         self.dynamixel_values[19] = self.dynamixel_values[19]*(1-k) + k*(512 + int((self.angles['Hz'] + np.deg2rad(80)) * 1024 / np.deg2rad(300)))
 
-        torque = [1] * 12 + [1]*7
+        torque = [1] * 13 + [1]*7
+
         self.sync_write_1(self.ADR_TOR_EN, torque)
         self.sync_write_1(self.ADR_SLOPE_CW, self.slope_CW)
         self.sync_write_1(self.ADR_SLOPE_CCW, self.slope_CCW)